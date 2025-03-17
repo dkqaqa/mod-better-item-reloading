db9d9389681348a591cc13caf2c7d1f056dce714
dkqaqa <123792340+dkqaqa@users.noreply.github.com>
2025-03-17 18:13:53 +0800

    Update BetterItemReloading.cpp

--- src/BetterItemReloading.cpp
+++ src/BetterItemReloading.cpp
@@ -130,7 +130,7 @@ public:
             for (uint8 i = 0; i < itemTemplate->StatsCount; ++i)
             {
                 itemTemplate->ItemStat[i].ItemStatType = uint32(fields[28 + i * 2].Get<uint8>());
-                itemTemplate->ItemStat[i].ItemStatValue = int32(fields[29 + i * 2].Get<int16>());
+                itemTemplate->ItemStat[i].ItemStatValue = int32(fields[29 + i * 2].Get<int32>());
             }
 
             itemTemplate->ScalingStatDistribution = uint32(fields[48].Get<uint16>());
@@ -595,7 +595,7 @@ public:
 
             }
 
-            handler->PSendSysMessage("Reloaded item template entry ({})", entry);
+            //handler->PSendSysMessage("Reloaded item template entry ({})", entry);
         }
 
         return true;
